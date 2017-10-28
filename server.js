f90b0369676d6eefb00cabc4fc904f1fd05358ed
mnteibel <30318729+mnteibel@users.noreply.github.com>
2017-10-28 17:23:48 -0500

    commit

--- server.js
+++ server.js
@@ -1,41 +1,27 @@
 // Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var htmlRoutes = require("./routes/htmlRoutes.js");
+var apiRoutes = require("./routes/apiRoutes.js")
 
-var express = require("express")
-var bodyParser = require("body-parser")
-var path = require("path")
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-// Sets up app
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-var app = express()
-var PORT = process.env.PORT || 3000
-
-// Sets up app data parsing
-
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
-
-// Friend Data
-
-// var friends = [
-// 	{
-// 		name: "Max Teibel",
-// 		answers: []
-// 	}
-// ]
-
-// Routes
-
-// Opening Page
-app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname, "start.html"))
-})
-app.get("/questions", function(req, res) {
-	res.sendFile(path.join(__dirname, "questions.html"))
-})
-
+htmlRoutes(app);
+apiRoutes(app);
 
 // Starts the server to begin listening
-
+// =============================================================
 app.listen(PORT, function() {
-	console.log("App listening on PORT " + PORT)
-})
+	console.log("App listening on PORT " + PORT);
+});
+
+
